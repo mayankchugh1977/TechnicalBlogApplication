93011bd88b21595fd185bdebcd73d1b9240109e4
Mayank <mayank.chugh@gmail.com>
2020-02-23 13:01:39 +0800

    Commit below older version

--- src/main/java/technicalblog/controller/PostController.java
+++ src/main/java/technicalblog/controller/PostController.java
@@ -1,4 +1,43 @@
 package technicalblog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import technicalblog.model.Post;
+import technicalblog.service.PostService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @RequestMapping("posts")
+    public String getUserPosts(Model model) {
+//        List<Post> posts = postService.getOnePost();
+
+        ArrayList <Post> posts = new ArrayList<>();
+
+        Post latestPost = postService.getOnePost();
+        posts.add(latestPost);
+
+        model.addAttribute("posts", posts);
+        return "posts";
+    }
+
+    @RequestMapping("/posts/newpost")
+    public String newPost() {
+        return "posts/create";
+    }
+
+    @RequestMapping(value = "/posts/create", method = RequestMethod.POST)
+    public String createPost(Post newPost) {
+        postService.createPost(newPost);
+        return "redirect:/posts";
+    }
 }
